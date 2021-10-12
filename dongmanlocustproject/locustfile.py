d5b920982860318b966d453ec9ce9995d68a6d30
huyq119 <yuqi.hu@gmail.com>
2021-10-12 14:38:43 +0800

    Update locustfile.py

--- dongmanlocustproject/locustfile.py
+++ dongmanlocustproject/locustfile.py
@@ -3,9 +3,21 @@ import time
 
 import yaml
 from locust import FastHttpUser, task, between, tag, events
-from locust.runners import MasterRunner
+from locust.runners import MasterRunner, WorkerRunner
 
 import getkey
+
+
+# Fired when the worker receives a message of type 'test_users'
+def setup_test_users(environment, msg, **kwargs):
+    for user in msg.data:
+        print(f"User {user['name']} received")
+    environment.runner.send_message('acknowledge_users', f"Thanks for the {len(msg.data)} users!")
+
+
+# Fired when the master receives a message of type 'acknowledge_users'
+def on_acknowledge(msg, **kwargs):
+    print(msg.data)
 
 
 @events.init.add_listener
@@ -14,11 +26,22 @@ def on_locust_init(environment, **kwargs):
         print("I'm on master node")
     else:
         print("I'm on a worker or standalone node")
+    if not isinstance(environment.runner, MasterRunner):
+        environment.runner.register_message('test_users', setup_test_users)
+    if not isinstance(environment.runner, WorkerRunner):
+        environment.runner.register_message('acknowledge_users', on_acknowledge)
 
 
 @events.test_start.add_listener
 def on_test_start(environment, **kwargs):
     print("开启测试")
+    if not isinstance(environment.runner, MasterRunner):
+        users = [
+            {"name": "User1"},
+            {"name": "User2"},
+            {"name": "User3"},
+        ]
+        environment.runner.send_message('test_users', users)
 
 
 @events.test_stop.add_listener
@@ -40,38 +63,38 @@ def get_body(neo_id):
     return body
 
 
-class RecommendPost(FastHttpUser):
-    host = "https://qarec.dongmanmanhua.cn"
-
-    @tag('tag1')
-    @task
-    def reporting_request_one(self):
-        id_list = yaml.safe_load(open("../dongmanlocustproject/neoid.yml"))
-        message = "/airec/behavior/data/reporting"
-        for neo_id in id_list:
-            body_content = get_body(neo_id)
-            r = self.client.post(message, json=body_content)
-            # print(r.text)
-            print("Response status code:", r.status_code)
-        wait_time = between(0.5, 10)
-
-    @tag('tag2')
-    @task
-    def reporting_request_two(self):
-        id_list = yaml.safe_load(open("../dongmanlocustproject/neoid.yml"))
-        message = "/airec/behavior/data/reporting"
-        for neo_id in id_list:
-            body_content = get_body(neo_id)
-            r = self.client.post(message, json=body_content)
-        wait_time = between(0.5, 10)
-
-# class EpisodeList(FastHttpUser):
-#     host = "http://qaapis.dongmanmanhua.cn"
+# class RecommendPost(FastHttpUser):
+#     host = "https://qarec.dongmanmanhua.cn"
 #
+#     @tag('tag1')
 #     @task
-#     def test_new(self):
-#         message = "/app/episode/list/v4?expires=1634972054459&language=zh-hans&locale=zh_CN&md5=EQAb0BWn7ohgVq" \
-#                   "-B_EGz4w&pageSize=214&platform=APP_IPHONE&serviceZone=CHINA&startIndex=0&titleNo=735&v=8 "
-#         r = self.client.get(message)
-#         print(r.status_code)
-#         wait_time = between(1, 5)
+#     def reporting_request_one(self):
+#         id_list = yaml.safe_load(open("../dongmanlocustproject/neoid.yml"))
+#         message = "/airec/behavior/data/reporting"
+#         for neo_id in id_list:
+#             body_content = get_body(neo_id)
+#             r = self.client.post(message, json=body_content)
+#             # print(r.text)
+#             print("Response status code:", r.status_code)
+#         wait_time = between(100, 200)
+#
+#     @tag('tag2')
+#     @task
+#     def reporting_request_two(self):
+#         id_list = yaml.safe_load(open("../dongmanlocustproject/neoid.yml"))
+#         message = "/airec/behavior/data/reporting"
+#         for neo_id in id_list:
+#             body_content = get_body(neo_id)
+#             r = self.client.post(message, json=body_content)
+#         wait_time = between(0.5, 10)
+
+class EpisodeList(FastHttpUser):
+    host = "http://qaapis.dongmanmanhua.cn"
+
+    @task
+    def test_new(self):
+        message = "/app/episode/list/v4?expires=1634972054459&language=zh-hans&locale=zh_CN&md5=EQAb0BWn7ohgVq" \
+                  "-B_EGz4w&pageSize=214&platform=APP_IPHONE&serviceZone=CHINA&startIndex=0&titleNo=735&v=8 "
+        r = self.client.get(message)
+        print(r.status_code)
+        wait_time = between(1, 5)
